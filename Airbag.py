35d00b2ddaaad83fbfda9772076d7761a24f4707
Labdoo User <anitabritez03@gmail.com>
2019-02-11 14:32:25 -0300

    segunda modificacion

--- Airbag.py
+++ Airbag.py
@@ -2,4 +2,27 @@ import adxl355 as acelerometro
 
 print(dir (acelerometro))
 
+"""
+Usage example for ADXL355 Python library
+This example prints on console the current values
+of axes on accelerometer
+"""
+
+import sys
+sys.path.append('../lib/')
+
+from adxl355 import ADXL355  # pylint: disable=wrong-import-position
+
+device = ADXL355()           # pylint: disable=invalid-name
+dimension = device.get_dimension()     # pylint: disable=invalid-name
+print(dimension) 
+
+while True:
+    dimension = device.get.dimension()
+    sleep(1)
+    if dimension x >130°:
+        gpio.output(led1,True)
+    else:
+        gpio.output(False)
+
 
